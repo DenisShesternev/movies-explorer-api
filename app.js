0269a7ea116b8b0997a573c2add8e81ecfbb86c6
Denis Shesternev <denshesternev@yandex.ru>
2023-11-03 08:44:12 +0800

    отправка на проверку 2

--- app.js
+++ app.js
@@ -4,17 +4,23 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
 const errorHandler = require('./middlewares/handleError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routes');
+const rateLimiter = require('./middlewares/rateLimiter');
 
-const { PORT = 3000, MONGODB = 'mongodb://127.0.0.1:27017/moviesAndUsersBD' } = process.env;
+const { PORT = 3000, MONGODB = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
+
+app.use(helmet());
+
+app.use(rateLimiter);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
