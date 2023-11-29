1af37ccc349bf42927f64c7c363b1951ae0338fa
Denis Shesternev <denshesternev@yandex.ru>
2023-11-29 12:48:52 +0800

    fix: исправлям валидацию

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routes');
 const rateLimiter = require('./middlewares/rateLimiter');
 
-const { PORT = 3001, MONGODB = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3000, MONGODB = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,16 +7,16 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new AuthError('Необходима авторизация');
+    throw new AuthError('Необходима авторизация.');
   }
 
   const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'yandex-praktikum');
+    payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'yandex-praktikum'}`);
   } catch (err) {
-    throw new AuthError('Необходима авторизация!!!');
+    throw new AuthError('Необходима авторизация!');
   }
   req.user = payload;
 
