5d7e65cc4e7144065290a5b4d24b47585f58ffef
Denis Shesternev <denshesternev@yandex.ru>
2023-12-11 13:30:30 +0800

    fix

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -56,7 +56,7 @@ const validationCreateMovie = celebrate({
 
 const validationDeleteMovie = celebrate({
   params: Joi.object().keys({
-    movieId: Joi.string().required().hex().length(24),
+    id: Joi.string().required().hex().length(24),
   }),
 });
 
